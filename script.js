ebd5f41e014f426b7f513166ae61eca728ea08a4
farrukhbaloch121212-ai <farrukhbaloch121212@gmail.com>
2026-08-30 04:43:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function calculateInterest() {
-    let principal = parseFloat(document.getElementById('principal').value);
-    let rate = Number(document.getElementById('rate').value);
-    let time = parseInt(document.getElementById('time').value);
 
-    if (isNaN(principal) || isNaN(rate) || isNaN(time)) {
-        return 0;
-    }
-
-    let interest = (principal * rate * time) / 100;
-    return interest;
-}
